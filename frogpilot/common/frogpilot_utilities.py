9de31fa623665fa369179f7092feb8d15eedb3e5
James <91348155+FrogAi@users.noreply.github.com>
2025-10-08 23:07:58 -0700

    Flash Panda

--- frogpilot/common/frogpilot_utilities.py
+++ frogpilot/common/frogpilot_utilities.py
@@ -24,6 +24,7 @@ running_threads = {}
 locks = {
   "backup_toggles": threading.Lock(),
   "download_theme": threading.Lock(),
+  "flash_panda": threading.Lock(),
   "update_checks": threading.Lock(),
   "update_openpilot": threading.Lock(),
 }
@@ -109,6 +110,20 @@ def extract_zip(zip_file, extract_path):
 
   zip_file.unlink()
   print(f"Extraction completed!")
+
+
+def flash_panda(params_memory):
+  for serial in Panda.list():
+    try:
+      with Panda(serial=serial) as panda:
+        print(f"Flashing Panda {serial}")
+        panda.reset(enter_bootstub=True)
+        panda.flash()
+    except Exception as exception:
+      print(f"Failed to flash Panda {serial}: {exception}")
+      sentry.capture_exception(exception)
+
+  params_memory.remove("FlashPanda")
 
 
 def is_url_pingable(url):
