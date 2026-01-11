cbe57c6a88b24838b6f35875af07d7ce7f3e671e
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-01-11 17:49:27 +0900

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -127,7 +127,7 @@ procs = [
   PythonProcess("joystick", "tools.joystick.joystick_control", and_(joystick, iscar)),
 
   # c3x lite
-  PythonProcess("beep", "selfdrive.controls.beep", always_run, enabled=TICI),  
+  PythonProcess("beep", "selfdrive.controls.beep", always_run),  
 ]
 
 # FrogPilot variables
